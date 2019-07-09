a721e379a5da66d5c7ef90190b8a53aff7225a2b
Kirk Shin <kirkshin@kirks-air.home>
2019-07-09 09:51:06 -0700

    [feat] add star rating document to schema

--- database/index.js
+++ database/index.js
@@ -1,28 +1,30 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/fec', {useNewUrlParser: true})
-  .then( () => console.log('Connected to mongodb'))
-  .catch( err => console.error('Unable to connect to mongodb'));
+mongoose.connect('mongodb://localhost/fec', { useNewUrlParser: true })
+  .then(() => console.log('Connected to mongodb'))
+  .catch(err => console.error('Unable to connect to mongodb'));
 
 const itemSchema = mongoose.Schema({
-  id: {type: Number, required: true},
-  brand: {type: String, required: true},
-  name: {type: String, required: true},
-  price: {type: Number, required: true},
+  id: { type: Number, required: true },
+  brand: { type: String, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
   sale: {
-    isOnSale: {type: Boolean, required: true},
-    percentOff: {type: Number, required: true}
+    isOnSale: { type: Boolean, required: true },
+    percentOff: { type: Number, required: true }
   },
-  isExclusive: {type: Boolean, required: true},
+  isExclusive: { type: Boolean, required: true },
   sizes: {
-    valueUs: {type: String, required: true},
-    valueUsEu: {type: String, required: true},
-    isUsEu: {type: Boolean, required: true},
-    isInStock: {type: Boolean, required: true}
+    valueUs: { type: String, required: true },
+    valueUsEu: { type: String, required: true },
+    isUsEu: { type: Boolean, required: true },
+    isInStock: { type: Boolean, required: true }
   },
-  images: {type: Array, required: true},
+  images: { type: Array, required: true },
+  starRating: { type: Number, required: true }
 });
 
 const Item = mongoose.model('Item', itemSchema);
 
 module.exports = Item;
+
