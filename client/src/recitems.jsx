5061ac505f7d6dd36c7ef6f7c0af376f45641f0f
Kirk Shin <kirkshin@Kirks-MacBook-Air.local>
2019-07-11 17:17:25 -0700

    [refactor] refactored to stateless component

--- client/src/recitems.jsx
+++ client/src/recitems.jsx
@@ -2,21 +2,12 @@ import React from 'react';
 import axios from 'axios';
 import Item from './item.jsx';
 
-class RecItems extends React.Component{
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-
-    return(
-      <div className='items-wrapper'>
-
-      {this.props.items.map( (item, index) => <Item  item={item} key={index} /> )}
-      </div>
-    )
-  }
-
+function RecItems(props) {
+  return (
+    <div className='items-wrapper'>
+      {props.items.map((item, index) => <Item item={item} key={index} />)}
+    </div>
+  )
 }
 
 export default RecItems;
